b48741b0d5eae6c176e643ce9763761005813146
Fran Lozano <franloza@ucm.es>
2016-03-20 16:56:29 +0100

    ROC Cuve added

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+import matplotlib.pyplot as plt
 from preprocessing import get_data
 from strategies import Strategies
 from sklearn import cross_validation as cv
 from sklearn.svm import LinearSVC, SVC
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, roc_curve, auc
 from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier, RadiusNeighborsClassifier
 from sklearn.grid_search import GridSearchCV
@@ -20,7 +21,7 @@ start_time = time()
 adjustment = False
 dimension_reduc = False
 additional_metrics = True
-selected_strategy = Strategies.KNeighbors
+selected_strategy = Strategies.RandomForest
 
 #==============================================================================
 
@@ -41,9 +42,9 @@ if (dimension_reduc):
 if (selected_strategy == Strategies.LinearSVC):
 	clf = LinearSVC()
 	if (adjustment):
-		parameters = {'C': [1, 10, 100, 1000], 'dual': [True, False], 
+		parameters = {'C': [1, 10, 100, 1000], 'dual': [True, False],
 		              'tol': [1e-4, 3e-3, 1e-3], 'max_iter': [50,100, 300, 500, 1000]}
-	else:	
+	else:
 		clf.set_params(max_iter=500, tol=0.0003, C=1, dual=True)
 
 # SVC (Non-linear kernels)
@@ -53,7 +54,7 @@ if (selected_strategy == Strategies.SVC):
 		parameters = [{'C': [1, 10, 100], 'kernel': ['rbf', 'sigmoid']},
 					  {'C': [1, 10, 100], 'kernel': ['poly'],
 					  'degree': [2, 3, 4, 5, 6]}]
-	else:	
+	else:
 		clf.set_params(kernel='rbf', C=100)
 
 #==============================================================================
@@ -66,7 +67,7 @@ if (selected_strategy == Strategies.KNeighbors):
 			      	  'weights': ['uniform', 'distance'],
 			      	  'algorithm': ['ball_tree', 'kd_tree', 'brute']}
 	else:
-		clf.set_params(n_neighbors=1, weights='uniform', algorithm='ball_tree', 
+		clf.set_params(n_neighbors=1, weights='uniform', algorithm='ball_tree',
 		               p=2, n_jobs=-1)
 
 if (selected_strategy == Strategies.RadiusNeighbors):
@@ -85,10 +86,10 @@ if (selected_strategy == Strategies.Bagging):
 		parameters = {'base_estimator': [None, KNeighborsClassifier],
 					  'max_samples': [0.25, 0.5, 0.75, 1],
 					  'max_features': [0.25, 0.5, 0.75, 1],
-					  'bootstrap': [True, False], 'n_jobs': [-1], 
+					  'bootstrap': [True, False], 'n_jobs': [-1],
 					  'bootstrap_features': [True, False]}
 	else:
-		clf = BaggingClassifier(KNeighborsClassifier(n_neighbors=1, 
+		clf = BaggingClassifier(KNeighborsClassifier(n_neighbors=1,
 		                                             weights='uniform', n_jobs=-1))
 
 #==============================================================================
@@ -131,21 +132,39 @@ if (adjustment):
 # 10-fold validation with given parameters & estimator
 if (not adjustment):
 	skf = cv.StratifiedKFold(series.target, n_folds=2, shuffle=True)
-	scores = cv.cross_val_score(clf, series.data, series.target, 
+	scores = cv.cross_val_score(clf, series.data, series.target,
 	                            cv=skf, scoring='roc_auc', n_jobs=-1)
-	
+
 	print('Scores are: ')
 	print('Min: {:.3f}%  Max: {:.3f}%  Avg: {:.3f}% (+/- {:.2f}%)'
-	      .format(scores.min() * 100, scores.max() * 100, scores.mean() * 100, 
+	      .format(scores.min() * 100, scores.max() * 100, scores.mean() * 100,
 	              scores.std() * 200))
 
 #==============================================================================
 
-'''
 if (additional_metrics):
+
+	# ROC CURVE
+
+	# Parameters
+	test_size = .5 #Prop. of validation examples
+
+	# Shuffle and split training and test sets
+	X_train, X_test, y_train, y_test = cv.train_test_split(series.data[:-1],
+	 				series.target[:-1], test_size= test_size,random_state=0)
+	y_score = clf.fit(X_train, y_train).predict_proba(X_test)
+
+	# Compute ROC curve and ROC area
+	fpr = dict()
+	tpr = dict()
+	roc_auc = dict()
+	fpr, tpr, _ = roc_curve(y_test, y_score[:, 1])
+	roc_auc = auc(fpr, tpr)
+	print (fpr,tpr)
+
 	# Plot of a ROC curve for a specific class
 	plt.figure()
-	plt.plot(fpr[2], tpr[2], label='ROC curve (area = %0.2f)' % roc_auc[2])
+	plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
 	plt.plot([0, 1], [0, 1], 'k--')
 	plt.xlim([0.0, 1.0])
 	plt.ylim([0.0, 1.05])
@@ -154,7 +173,6 @@ if (additional_metrics):
 	plt.title('Receiver operating characteristic example')
 	plt.legend(loc="lower right")
 	plt.show()
-'''
 
 #==============================================================================
 
