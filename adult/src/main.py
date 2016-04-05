8bb4fd980fe93d7e2865b37e58ed13521a74eabc
AlvarBer <alvaro.garcia95@hotmail.com>
2016-04-05 11:38:43 +0200

    Made syntax/indentation a bit more pythonic

--- adult/src/main.py
+++ adult/src/main.py
@@ -34,171 +34,172 @@ selected_strategy = Strategies.XGB
 #==============================================================================
 
 # Dimensionality Reduction
-if (dimension_reduc):
-	series.data = RandomizedPCA(n_components=150).fit_transform(series.data)
-	print('Reduced Data size: {}'.format(series.data.shape))
+if dimension_reduc:
+    series.data = RandomizedPCA(n_components=150).fit_transform(series.data)
+    print('Reduced Data size: {}'.format(series.data.shape))
 
 #==============================================================================
 
 # Support Vector Machines
 # Linear SVC
-if (selected_strategy == Strategies.LinearSVC):
-	clf = LinearSVC()
-	if (adjustment):
-		parameters = {'C': [1, 10, 100, 1000], 'dual': [True, False],
-		              'tol': [1e-4, 3e-3, 1e-3], 'max_iter': [50,100, 300, 500, 1000]}
-	else:
-		clf.set_params(max_iter=500, tol=0.0003, C=1, dual=True)
+if selected_strategy == Strategies.LinearSVC:
+    clf = LinearSVC()
+    if (adjustment):
+        parameters = {'C': [1, 10, 100, 1000], 'dual': [True, False],
+                      'tol': [1e-4, 3e-3, 1e-3], 'max_iter': [50,100, 300, 500, 1000]}
+    else:
+        clf.set_params(max_iter=500, tol=0.0003, C=1, dual=True)
 
 # SVC (Non-linear kernels)
-if (selected_strategy == Strategies.SVC):
-	clf = SVC()
-	if (adjustment):
-		parameters = [{'C': [1, 10, 100], 'kernel': ['rbf', 'sigmoid']},
-					  {'C': [1, 10, 100], 'kernel': ['poly'],
-					  'degree': [2, 3, 4, 5, 6]}]
-	else:
-		clf.set_params(kernel='rbf', C=100)
+if selected_strategy == Strategies.SVC:
+    clf = SVC()
+    if (adjustment):
+        parameters = [{'C': [1, 10, 100], 'kernel': ['rbf', 'sigmoid']},
+                      {'C': [1, 10, 100], 'kernel': ['poly'],
+                      'degree': [2, 3, 4, 5, 6]}]
+    else:
+        clf.set_params(kernel='rbf', C=100)
 
 #==============================================================================
 
 # KNeighbors / RadiusNeighbors
-if (selected_strategy == Strategies.KNeighbors):
-	clf = KNeighborsClassifier()
-	if (adjustment):
-		parameters = {'n_neighbors': [1,3,5],  'p': [1, 2],
-			      	  'weights': ['uniform', 'distance'],
-			      	  'algorithm': ['ball_tree', 'kd_tree', 'brute']}
-	else:
-		clf.set_params(n_neighbors=1, weights='uniform', algorithm='ball_tree',
-		               p=2, n_jobs=-1)
+if selected_strategy == Strategies.KNeighbors:
+    clf = KNeighborsClassifier()
+    if (adjustment):
+        parameters = {'n_neighbors': [1,3,5],  'p': [1, 2],
+                      'weights': ['uniform', 'distance'],
+                      'algorithm': ['ball_tree', 'kd_tree', 'brute']}
+    else:
+        clf.set_params(n_neighbors=1, weights='uniform', algorithm='ball_tree',
+                       p=2, n_jobs=-1)
 
-if (selected_strategy == Strategies.RadiusNeighbors):
-	clf = RadiusNeighborsClassifier()
-	if (adjustment):
-		parameters = {'radius': [1, 2, 3, 5, 10, 25],
-					  'weights': ['uniform', 'distance']}
-	else:
-		clf.set_params(radius=1, weights='distance')
+if selected_strategy == Strategies.RadiusNeighbors:
+    clf = RadiusNeighborsClassifier()
+    if (adjustment):
+        parameters = {'radius': [1, 2, 3, 5, 10, 25],
+                      'weights': ['uniform', 'distance']}
+    else:
+        clf.set_params(radius=1, weights='distance')
 
 #==============================================================================
 
 # Bagging Meta-Classifier
-if (selected_strategy == Strategies.Bagging):
-	clf = BaggingClassifier(RandomForestClassifier())
-	if (adjustment):
-		parameters = {'n_estimators' : [200]}
-	else:
-		clf.set_params(n_estimators=10,
-		               base_estimator=RandomForestClassifier(n_estimators=200,
-		                                        			   n_jobs=-1))
+if selected_strategy == Strategies.Bagging:
+    clf = BaggingClassifier(RandomForestClassifier())
+    if (adjustment):
+        parameters = {'n_estimators' : [200]}
+    else:
+        clf.set_params(n_estimators=10,
+                       base_estimator=RandomForestClassifier(n_estimators=200,
+                                                               n_jobs=-1))
 
 #==============================================================================
 
 # Trees
 # Random Forest
-if (selected_strategy == Strategies.RandomForest):
-	clf = RandomForestClassifier()
-	if(adjustment):
-		parameters = {'n_estimators':[200]}
-	else:
-		clf = RandomForestClassifier()
+if selected_strategy == Strategies.RandomForest:
+    clf = RandomForestClassifier()
+    if(adjustment):
+        parameters = {'n_estimators':[200]}
+    else:
+        clf = RandomForestClassifier()
 
 # Decision Tree
-if (selected_strategy == Strategies.DecisionTree):
-	clf = DecisionTreeClassifier()
-	if(adjustment):
-		parameters = {}
-	else:
-		clf = DecisionTreeClassifier(n_estimators=200)
+if selected_strategy == Strategies.DecisionTree:
+    clf = DecisionTreeClassifier()
+    if adjustment:
+        parameters = {}
+    else:
+        clf = DecisionTreeClassifier(n_estimators=200)
 
 #==============================================================================
 
 # Gaussian Bayes
-if (selected_strategy == Strategies.GaussianBayes):
-	clf = GaussianNB()
-	if(adjustment):
-		parameters = {}
-	else:
-		clf = GaussianNB()
+if selected_strategy == Strategies.GaussianBayes:
+    clf = GaussianNB()
+    if adjustment:
+        parameters = {}
+    else:
+        clf = GaussianNB()
 
 #==============================================================================
-# XGB Classifier
-if (selected_strategy == Strategies.XGB):
-	clf = XGBClassifier()
 
-	if(adjustment):
-		parameters = {'colsample_bytree' : [0.5,0.8,0.9]}
-	else:
-		clf.set_params(max_depth=6, learning_rate=0.1, n_estimators=350, \
-		min_child_weight = 2, gamma= 0.2,colsample_bytree=0.9)
+# XGB Classifier
+if selected_strategy == Strategies.XGB:
+    clf = XGBClassifier()
+    if adjustment:
+        parameters = {'colsample_bytree' : [0.5, 0.8, 0.9]}
+    else:
+        clf.set_params(max_depth=6, learning_rate=0.1, n_estimators=350, 
+                       min_child_weight = 2, gamma= 0.2, colsample_bytree=0.9)
 
 #==============================================================================
 
 # Adjustment
-if (adjustment):
-	skf = cv.StratifiedKFold(series.target, n_folds=3, shuffle=True)
-	grid_search = GridSearchCV(clf, parameters, scoring='roc_auc', cv=skf, n_jobs=-1)
-	grid_search.fit(series.data, series.target)
-	print('Best score: {:.3f}%'.format(grid_search.best_score_ * 100))
-	print('Best parameters set: {}'.format(grid_search.best_params_))
+if adjustment:
+    skf = cv.StratifiedKFold(series.target, n_folds=3, shuffle=True)
+    grid_search = GridSearchCV(clf, parameters, scoring='roc_auc', cv=skf, n_jobs=-1)
+    grid_search.fit(series.data, series.target)
+    print('Best score: {:.3f}%'.format(grid_search.best_score_ * 100))
+    print('Best parameters set: {}'.format(grid_search.best_params_))
 
-	if (export_submission):
-		pred = grid_search.predict(test)
+    if export_submission:
+        pred = grid_search.predict(test)
 
 #==============================================================================
 
 # n-fold validation with given parameters & estimator
-if (not adjustment):
-	skf = cv.StratifiedKFold(series.target, n_folds=3, shuffle=True)
-	scores = cv.cross_val_score(clf, series.data, series.target,
-	                            cv=skf, scoring='roc_auc', n_jobs=-1)
+if not adjustment:
+    skf = cv.StratifiedKFold(series.target, n_folds=3, shuffle=True)
+    scores = cv.cross_val_score(clf, series.data, series.target,
+                                cv=skf, scoring='roc_auc', n_jobs=-1)
 
-	print('Scores are: ')
-	print('Min: {:.3f}%  Max: {:.3f}%  Avg: {:.3f}% (+/- {:.2f}%)'
-	      .format(scores.min() * 100, scores.max() * 100, scores.mean() * 100,
-	              scores.std() * 200))
-	if (export_submission):
-		pred = clf.fit(series.data, series.target).predict(test)
+    print('Scores are: ')
+    print('Min: {:.3f}%  Max: {:.3f}%  Avg: {:.3f}% (+/- {:.2f}%)'
+          .format(scores.min() * 100, scores.max() * 100, scores.mean() * 100,
+                  scores.std() * 200))
+    if (export_submission):
+        pred = clf.fit(series.data, series.target).predict(test)
 
 #==============================================================================
 
-if (additional_metrics):
-	# ROC CURVE
+if additional_metrics:
+    # ROC CURVE
 
-	# Parameters
-	test_size = .5 #Prop. of validation examples
+    # Parameters
+    test_size = .5 #Prop. of validation examples
 
-	# Shuffle and split training and test sets
-	X_train, X_test, y_train, y_test = cv.train_test_split(series.data[:-1],
-	 				series.target[:-1], test_size= test_size,random_state=0)
-	y_score = clf.fit(X_train, y_train).predict(X_test)
+    # Shuffle and split training and test sets
+    X_train, X_test, y_train, y_test = cv.train_test_split(series.data[:-1],
+                    series.target[:-1], test_size= test_size,random_state=0)
+    y_score = clf.fit(X_train, y_train).predict(X_test)
 
-	# Compute ROC curve and ROC area
-	fpr = dict()
-	tpr = dict()
-	roc_auc = dict()
-	fpr, tpr, _ = roc_curve(y_test, y_score)
-	roc_auc = auc(fpr, tpr)
+    # Compute ROC curve and ROC area
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    fpr, tpr, _ = roc_curve(y_test, y_score)
+    roc_auc = auc(fpr, tpr)
 
-	# Plot of a ROC curve for a specific class
-	plt.figure()
-	plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
-	plt.plot([0, 1], [0, 1], 'k--')
-	plt.xlim([0.0, 1.0])
-	plt.ylim([0.0, 1.05])
-	plt.xlabel('False Positive Rate')
-	plt.ylabel('True Positive Rate')
-	plt.title('Receiver operating characteristic example')
-	plt.legend(loc="lower right")
-	plt.show()
+    # Plot of a ROC curve for a specific class
+    plt.figure()
+    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
 
 #==============================================================================
 
 print('Time elapsed: {}'.format(timedelta(seconds=(time() - start_time))))
 
 #==============================================================================
-if (export_submission):
-	print('Exporting prediction for submission...')
-	submission = pd.DataFrame({"TARGET": pred})
-	submission.to_csv("../data/submission.csv", index=False, header=False)
+
+if export_submission:
+    print('Exporting prediction for submission...')
+    submission = pd.DataFrame({"TARGET": pred})
+    submission.to_csv("../data/submission.csv", index=False, header=False)
--- adult/src/preprocessing.py
+++ adult/src/preprocessing.py
@@ -3,76 +3,77 @@ from sklearn.preprocessing import LabelEncoder
 from collections import namedtuple
 
 def get_data(load_test=False):
-	#Load and process the training data
-	print('Loading and processing training data...')
-	path = '../data/train.csv'
-	values = process_data(path)
+    # Load and process the training data
+    print('Loading and processing training data...')
+    path = '../data/train.csv'
+    values = process_data(path)
 
-	#Split features and label
-	data, target = values[:,:-1], values[:,-1:]
-	series = namedtuple('Series', ['data', 'target'])
-	series.data = data
-	series.target = target.ravel()
+    # Split features and label
+    data, target = values[:,:-1], values[:,-1:]
+    series = namedtuple('Series', ['data', 'target'])
+    series.data = data
+    series.target = target.ravel()
 
-	if (load_test):
-		print('Loading and processing test data...')
-		path = '../data/test.csv'
-		test = process_data(path,proc_label=False)
+    if (load_test):
+        print('Loading and processing test data...')
+        path = '../data/test.csv'
+        test = process_data(path,proc_label=False)
 
-	return [series,test]
+    return [series,test]
 
-def process_data(path,proc_label=True):
-	df = pandas.read_csv(path, header=0)
-	le = LabelEncoder()
+def process_data(path, proc_label=True):
+    df = pandas.read_csv(path, header=0)
+    le = LabelEncoder()
 
-	#Drop ID column
-	df = df.drop(df.columns[0], axis=1)
+    # Drop ID column
+    df = df.drop(df.columns[0], axis=1)
 
-	# Convert all the categorical data to numerical values
-	df.workclass = le.fit_transform(df.workclass)
-	df.education = le.fit_transform(df.education)
-	df['marital-status'] = le.fit_transform(df['marital-status'])
-	df.occupation = le.fit_transform(df.occupation)
-	df.relationship = le.fit_transform(df.relationship)
-	df.race = le.fit_transform(df.race)
-	df.sex = le.fit_transform(df.sex)
+    # Convert all the categorical data to numerical values
+    df.workclass = le.fit_transform(df.workclass)
+    df.education = le.fit_transform(df.education)
+    df['marital-status'] = le.fit_transform(df['marital-status'])
+    df.occupation = le.fit_transform(df.occupation)
+    df.relationship = le.fit_transform(df.relationship)
+    df.race = le.fit_transform(df.race)
+    df.sex = le.fit_transform(df.sex)
 
-	#Capital gain mapping (Decreases score)
-	#df['capital-gain'] = df['capital-gain'].apply(map_capital_gain)
+    # Capital gain mapping (Decreases score)
+    #df['capital-gain'] = df['capital-gain'].apply(map_capital_gain)
 
-	#Native country mapping
-	df['native-country'] = df['native-country'].apply(map_native_country)
+    # Native country mapping
+    df['native-country'] = df['native-country'].apply(map_native_country)
 
-	#Process the label if it's training data
-	if(proc_label):
-		df.income = le.fit_transform(df.income)
+    # Process the label if it's training data
+    if(proc_label):
+        df.income = le.fit_transform(df.income)
 
-	return df.values
+    return df.values
 
 def map_native_country(country):
-	#United-States or None
-	if country in ['United-States','None']:
-		return 0
+    # United-States or None
+    if country in ['United-States','None']:
+        return 0
 
-	#Developed Countries
-	if country in ['England','Canada','Germany','Japan','Poland','China','Italy', \
-	'Mexico','Portugal','Ireland','France','Holand-Netherlands','Greece','Hungary', \
-	'Scotland']:
-		return 1
-	#Underdeveloped Countries
-	if country in ['Cambodia','Puerto-Rico','Outlying-US(Guam-USVI-etc)','India', \
-	'South','Cuba','Iran','Honduras','Philippines','Jamaica','Vietnam','Dominican-Republic', \
-	'Laos','Ecuador','Taiwan','Haiti','Columbia','Nicaragua','Thailand','Peru', \
-	'Hong','Trinadad&Tobago','Guatemala','Yugoslavia','El-Salvador']:
-		return 2
+    # Developed Countries
+    if country in ['England', 'Canada', 'Germany', 'Japan', 'Poland', 'China', 
+                   'Italy', 'Mexico', 'Portugal', 'Ireland', 'France', 
+                   'Holand-Netherlands', 'Greece', 'Hungary', 'Scotland']:
+        return 1
+    # Underdeveloped Countries
+    if country in ['Cambodia', 'Puerto-Rico', 'Outlying-US(Guam-USVI-etc)', 'India',
+                   'South', 'Cuba', 'Iran', 'Honduras', 'Philippines', 'Jamaica', 
+                   'Vietnam', 'Dominican-Republic', 'Laos', 'Ecuador', 'Taiwan', 
+                   'Haiti', 'Columbia', 'Nicaragua', 'Thailand', 'Peru', 'Hong', 
+                   'Trinadad&Tobago', 'Guatemala', 'Yugoslavia', 'El-Salvador']:
+        return 2
 
 
 def map_capital_gain(amount):
-	if(amount > 10000):
-		return 4
-	elif(amount > 5000):
-		return 3
-	elif(amount > 1000):
-		return 2
-	else:
-		return 0
+    if amount > 10000:
+        return 4
+    elif amount > 5000:
+        return 3
+    elif amount > 1000:
+        return 2
+    else:
+        return 0
